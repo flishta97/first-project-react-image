1278ae822dc0990571feccc90700f23e05cc5acf
fereshteh <59455405+flishta97@users.noreply.github.com>
2020-05-06 01:15:32 +0430

    image project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React ,{Component} from 'react';
+import Flower from './HelloWord/Flower';
+
+import './App.css';
+
+class App extends Component{
+  render(){
+  return (
+    <div className="App">
+  <Flower/>
+    </div>
+  );
+}
+}
+export default App;
---
+++ src/HelloWord/Flower.js
@@ -0,0 +1,14 @@
+import React ,{Component} from 'react';
+
+class Flower extends Component{
+render(){
+  return (
+    <div >
+   
+
+    </div>
+
+  );
+}}
+
+export default Flower;
